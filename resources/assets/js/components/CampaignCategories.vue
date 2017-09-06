8bbc692098de27b5534764423404ad230d0913fe
halidm <halid.mousa@gmail.com>
2017-09-06 18:19:30 +0400

    z-index of modals

--- resources/assets/js/components/CampaignCategories.vue
+++ resources/assets/js/components/CampaignCategories.vue
@@ -1,14 +1,15 @@
 <template>
-    <div>
-        <div class="col-sm-12">
-            <legend><i class="fa fa-fw fa-star"></i> Categories</legend>
-            <tk-select-list v-model="campaign.cat.data">
-                <tk-select v-for="category in $parent.$parent.$parent.categoriesList" :icon="'iabicon iabicon-' + category.code | lowercase" :trueValue="category.code" :subtitle="category.code">
-                    {{ category.type }}
-                </tk-select>
-            </tk-select-list>
-        </div>
-    </div>
+    <v-container fluid grid-list-md>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-checkbox v-for="category in $parent.$parent.$parent.categoriesList" 
+                v-model="campaign.cat.data"  
+                :label="category.type"
+                :value="category.code">
+                </v-checkbox>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
