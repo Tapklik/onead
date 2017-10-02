07242c05517d087585df05a745587043d3310e5e
EmirSila <emirsilajdzic16@gmail.com>
2017-10-02 11:40:38 +0200

    Change fonts from http:// to //

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,9 +15,9 @@
 
     <link rel="stylesheet" href="/css/materialdesignicons.min.css" />
 
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
 
-    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+    <link href='//fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
 
 
     <script>
